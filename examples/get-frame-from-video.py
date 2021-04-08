73f9be8f39aea3d404abce734384a6339577634e
Andrea Ranieri <andrea.ranieri@cnr.it>
2021-04-08 18:38:10 +0200

    Now get-frame-from-video.py runs in a nice loop...

--- examples/get-frame-from-video.py
+++ examples/get-frame-from-video.py
@@ -23,44 +23,51 @@ depth_video_fname = args.depth_video_filename
 #color_video_fname = 'color-2021-04-08-16-26-06.h265'
 #depth_video_fname = 'depth-2021-04-08-16-26-06.h265'
 
+color_cap = depth_cap = None
+
 def get_num_frames(video_fname):
 	cap = cv2.VideoCapture(video_fname)
 	num_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-	return num_frames
+	return cap, num_frames
 
-def get_frame_no(video_fname, frame_number):
-	cap = cv2.VideoCapture(video_fname)
+def get_frame_no(cap, frame_number):
+	#cap = cv2.VideoCapture(video_fname)
 	cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number-1)
 	res, frame = cap.read()
 	return res, frame
 
-color_num_frames  = get_num_frames(color_video_fname)
-print(f'{color_num_frames = }')
+color_cap, color_num_frames = get_num_frames(color_video_fname)
+depth_cap, depth_num_frames = get_num_frames(depth_video_fname)
+print(f'{color_num_frames = } - {depth_num_frames = }')
+if color_num_frames != depth_num_frames:
+	print(f'-------------------------------------------------------------------')
+	print(f'Warning! The number of frames in color and depth video file differ!')
+	print(f'-------------------------------------------------------------------')
 if color_num_frames <= 0:
 	print(f'Error retrieving the number of frames in color video file: {color_video_fname}')
 	sys.exit(0)
 
-random_frame_no   = randrange(color_num_frames)
-print(f'{color_num_frames = } - {random_frame_no = }')
-cres, color_frame = get_frame_no(color_video_fname, random_frame_no)
-print(f'{cres = } - {color_frame.shape = }')
+while True:
+	random_frame_no   = randrange(color_num_frames)
+	print(f'{color_num_frames = } - {random_frame_no = }')
 
-depth_num_frames  = get_num_frames(depth_video_fname)
-print(f'{depth_num_frames = } - {random_frame_no = }')
-cres, depth_frame = get_frame_no(depth_video_fname, random_frame_no)
-print(f'{cres = } - {depth_frame.shape = }')
+	cres, color_frame = get_frame_no(color_cap, random_frame_no)
+	print(f'{cres = } - {color_frame.shape = }')
+	
+	cres, depth_frame = get_frame_no(depth_cap, random_frame_no)
+	print(f'{cres = } - {depth_frame.shape = }')
+	
+	color_width, color_height, color_ch = color_frame.shape
+	depth_width, depth_height, depth_ch = depth_frame.shape
+	cv2.namedWindow ('rgb',		cv2.WINDOW_NORMAL)
+	cv2.resizeWindow('rgb',		int(color_width/2), int(color_height/2))
+	cv2.namedWindow ('disparity',	cv2.WINDOW_NORMAL)
+	cv2.resizeWindow('disparity',	int(color_width/2), int(color_height/2))
 
-color_width, color_height, color_ch = color_frame.shape
-depth_width, depth_height, depth_ch = depth_frame.shape
-
-cv2.namedWindow('rgb',cv2.WINDOW_NORMAL)
-cv2.resizeWindow('rgb', int(color_width/2), int(color_height/2))
-
-cv2.namedWindow('disparity',cv2.WINDOW_NORMAL)
-cv2.resizeWindow('disparity', int(color_width/2), int(color_height/2))
-
-while cv2.waitKey(0) & 0xFF != ord('q'):
 	#cv2.imshow(video_fname+' frame '+ str(frame_seq), frame)
 	cv2.imshow('rgb',	color_frame)
 	cv2.imshow('disparity',	depth_frame)
 
+	key = cv2.waitKey(0)
+	if key & 0xFF == ord('q'):
+		break
