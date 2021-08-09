8e9d2fbab909d8a16e499e32db97c09142d1ca1e
Avio <andreran@gmail.com>
2021-08-09 23:13:37 +0200

    Added --videofile, --rotatevideo and --outputvideo options

--- examples/custom-scripts/road-cracks-semantic-segmentation.py
+++ examples/custom-scripts/road-cracks-semantic-segmentation.py
@@ -5,6 +5,11 @@ import depthai as dai
 import numpy as np
 import argparse
 import time
+
+import shlex
+import subprocess as sp
+
+from time import monotonic
 from datetime import datetime, timedelta
 
 '''
@@ -22,14 +27,19 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-shape", "--nn_shape", help="select NN model shape", default=256, type=int)
 #parser.add_argument("-nn", "--nn_path", help="select model path for inference", default='models/deeplab_v3_plus_mvn2_decoder_256_openvino_2021.2_6shave.blob', type=str)
 #parser.add_argument("-nn", "--nn_path", help="select model path for inference", default='models/pothole-segmentron-deeplabv3+-resnet50-no-data-aug-img_size-360-640-2b-2021-08-06_04.49.43-WD-0.0001-BS-8-LR-1e-07-1e-06-epoch-5-dice_multi-0.8379.blob', type=str)
-parser.add_argument("-nn", "--nn_path", help="select model path for inference", default='models/pothole-segmentron-deeplabv3+-mobilenet_v2-basic-data-aug-img_size-360-640-2b-2021-08-06_13.07.29-WD-0.0001-BS-8-LR-1e-07-1e-06-epoch-1-dice_multi-0.8192.blob', type=str)
+#parser.add_argument("-nn", "--nn_path", help="select model path for inference", default='models/pothole-segmentron-deeplabv3+-mobilenet_v2-basic-data-aug-img_size-360-640-2b-2021-08-06_13.07.29-WD-0.0001-BS-8-LR-1e-07-1e-06-epoch-1-dice_multi-0.8192.blob', type=str)
+parser.add_argument("-nn", "--nn_path", help="select model path for inference", default='models/pothole-segmentron-deeplabv3+-mobilenet_v2-no-data-aug-img_size-180-320-1b-2021-08-09_14.52.21-WD-0.0001-BS-48-LR-1e-07-1e-06-epoch-2-dice_multi-0.8823.blob', type=str)
+parser.add_argument("-vf", "--videofile",   help="specify a video file to perform inference on instead of video stream", default='', type=str)
+parser.add_argument("-r",  "--rotatevideo", help="rotate 90° the input video for inference (e.g. vertical mobile phone video), then rotate it back for display and/or saving", default='False', type=str)
+parser.add_argument("-ov", "--outputvideo", help="specify a video file where to record the output", default='', type=str)
 args = parser.parse_args()
 
 #nn_shape = args.nn_shape
 if '256' in args.nn_path:
 	nn_img_size = (256,256)
 else:
-	nn_img_size = (640,360)
+	#nn_img_size = (640,360)
+	nn_img_size = (320,180)
 nn_path = args.nn_path
 #TARGET_SHAPE = (400,400)
 
@@ -63,8 +73,14 @@ def get_multiplier(output_tensor, new_shape):
 	output_colors = np.take(class_binary, output, axis=0)
 	return output_colors
 
-def show_deeplabv3p(output_colors, frame):
-	return cv2.addWeighted(frame,1, output_colors,0.5,0)
+def show_deeplabv3p(output_colors, frame, debug=False):
+	if debug:
+		print(f'show_deeplabv3p() - {frame.shape = } - {output_colors.shape}')
+	if frame.shape != output_colors.shape:
+		frame = cv2.resize(frame, (output_colors.shape[1], output_colors.shape[0]))
+	if debug:
+		print(f'show_deeplabv3p() - {frame.shape = } - {output_colors.shape}')
+	return cv2.addWeighted(frame, 1, output_colors, 0.5, 0)
 
 def dispay_colored_depth(frame, name):
 	frame_colored = cv2.normalize(frame, None, 255, 0, cv2.NORM_INF, cv2.CV_8UC1)
@@ -112,6 +128,10 @@ def crop_to_square(frame):
 	# print(height, width, delta)
 	return frame[0:height, delta:width-delta]
 
+def to_planar(arr: np.ndarray, shape: tuple) -> np.ndarray:
+	return cv2.resize(arr, shape).transpose(2, 0, 1).flatten()
+
+
 # Start defining a pipeline
 pipeline = dai.Pipeline()
 
@@ -120,72 +140,118 @@ if '2021.2' in args.nn_path:
 else:
 	pipeline.setOpenVINOVersion(version=dai.OpenVINO.Version.VERSION_2021_3)
 
-cam = pipeline.createColorCamera()
-cam.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
-# Color cam: 1920x1080
-# Mono cam: 640x400
-cam.setIspScale(2,3) # To match 400P mono cameras
-cam.setBoardSocket(dai.CameraBoardSocket.RGB)
-cam.initialControl.setManualFocus(130)
-
-
-# For deeplabv3
-cam.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
-#cam.setPreviewSize(int(nn_shape/2), int(nn_shape/2))
-cam.setPreviewSize(nn_img_size)
-cam.setInterleaved(False)
-
-# NN output linked to XLinkOut
-isp_xout = pipeline.createXLinkOut()
-isp_xout.setStreamName("cam")
-cam.isp.link(isp_xout.input)
-
-
 # Define a neural network that will make predictions based on the source frames
 detection_nn = pipeline.createNeuralNetwork()
 detection_nn.setBlobPath(nn_path)
 detection_nn.input.setBlocking(False)
 detection_nn.setNumInferenceThreads(2)
-cam.preview.link(detection_nn.input)
 
 # NN output linked to XLinkOut
 xout_nn = pipeline.createXLinkOut()
 xout_nn.setStreamName("nn")
 detection_nn.out.link(xout_nn.input)
 
-xout_passthrough = pipeline.createXLinkOut()
-xout_passthrough.setStreamName("pass")
-# Only send metadata, we are only interested in timestamp, so we can sync
-# depth frames with NN output
-# xout_passthrough.setMetadataOnly(True)
-detection_nn.passthrough.link(xout_passthrough.input)
+if args.videofile:
+	xinFrame = pipeline.createXLinkIn()
+	xinFrame.setStreamName("inFrame")
+	xinFrame.out.link(detection_nn.input)
+else:
+	cam = pipeline.createColorCamera()
+	cam.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+	# Color cam: 1920x1080
+	# Mono cam: 640x400
+	cam.setIspScale(2,3) # To match 400P mono cameras
+	cam.setBoardSocket(dai.CameraBoardSocket.RGB)
+	cam.initialControl.setManualFocus(130)
+	
+	
+	# For deeplabv3
+	cam.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
+	#cam.setPreviewSize(int(nn_shape/2), int(nn_shape/2))
+	cam.setPreviewSize(nn_img_size)
+	cam.setInterleaved(False)
+
+	# NN output linked to XLinkOut
+	isp_xout = pipeline.createXLinkOut()
+	isp_xout.setStreamName("cam")
+	cam.isp.link(isp_xout.input)
+
+	cam.preview.link(detection_nn.input)
+
+	xout_passthrough = pipeline.createXLinkOut()
+	xout_passthrough.setStreamName("pass")
+	# Only send metadata, we are only interested in timestamp, so we can sync
+	# depth frames with NN output
+	# xout_passthrough.setMetadataOnly(True)
+	detection_nn.passthrough.link(xout_passthrough.input)
 
 debug = False
 
 # Pipeline is defined, now we can connect to the device
 with dai.Device(pipeline) as device:
-	# Output queues will be used to get the outputs from the device
-	q_color = device.getOutputQueue(name="cam", maxSize=4, blocking=False)
+
+	if args.videofile:
+		# Input queue will be used to send video frames to the device.
+		qIn = device.getInputQueue(name="inFrame")
+	else:
+		# Output queues will be used to get the outputs from the device
+		q_color = device.getOutputQueue(name="cam",  maxSize=4, blocking=False)
+		q_pass  = device.getOutputQueue(name="pass", maxSize=4, blocking=False)
+
 	#q_depth = device.getOutputQueue(name="depth", maxSize=4, blocking=False)
-	q_nn = device.getOutputQueue(name="nn", maxSize=4, blocking=False)
-	q_pass = device.getOutputQueue(name="pass", maxSize=4, blocking=False)
+	q_nn   = device.getOutputQueue(name="nn",   maxSize=4, blocking=False)
 
 	fps = FPSHandler()
 	sync = HostSync()
 	depth_frame = None
 
-	while True:
+	if args.videofile:
+		cap       = cv2.VideoCapture(args.videofile)
+		is_opened = cap.isOpened()
+		if args.outputvideo:
+			if args.rotatevideo != 'False':
+				#out_cap = cv2.VideoWriter(args.outputvideo, cv2.VideoWriter.fourcc('M','J','P','G'), 60, nn_img_size)
+				process = sp.Popen(shlex.split(f'ffmpeg -y -s {nn_img_size[1]}x{nn_img_size[0]} -pixel_format bgr24 -f rawvideo -r 60 -i pipe: -vcodec libx264 -pix_fmt yuv420p -crf 24 {args.outputvideo}'), stdin=sp.PIPE)
+			else:
+				#out_cap = cv2.VideoWriter(args.outputvideo, cv2.VideoWriter.fourcc('M','J','P','G'), 60, nn_img_size[::-1])
+				process = sp.Popen(shlex.split(f'ffmpeg -y -s {nn_img_size[0]}x{nn_img_size[1]} -pixel_format bgr24 -f rawvideo -r 60 -i pipe: -vcodec libx264 -pix_fmt yuv420p -crf 24 {args.outputvideo}'), stdin=sp.PIPE)
+			#if not out_cap.isOpened():
+			#	print(f'Failed to open OpenCV video writer: {args.outputvideo}')
+			#	sys.exit(0)
+	else:
+		is_opened = True
+
+	while is_opened:
+		if args.videofile:
+			read_correctly, frame = cap.read()
+	
+			if not read_correctly:
+				break
+
+			if args.rotatevideo != 'False':
+				frame = frame.transpose(1, 0, 2)
+
+			img = dai.ImgFrame()
+			img.setData(to_planar(frame, nn_img_size))
+			img.setTimestamp(monotonic())
+			img.setWidth(nn_img_size[0])
+			img.setHeight(nn_img_size[1])
+			qIn.send(img)
 		'''
 		in_depth = q_depth.tryGet()
 		if in_depth is not None:
 			sync.add_msg("depth", in_depth)
 		'''
 
-		in_nn = q_nn.tryGet()
+		if not args.videofile:
+			in_nn = q_nn.tryGet()
+		else:
+			in_nn = q_nn.get()
 		if in_nn is not None:
 			fps.next_iter()
 			# Get NN passthrough frame
-			frame = q_pass.get().getCvFrame()
+			if not args.videofile:
+				frame = q_pass.get().getCvFrame()
 
 			if debug:
 				print(f'{in_nn = }')
@@ -208,7 +274,8 @@ with dai.Device(pipeline) as device:
 			else:
 				layer1 = in_nn.getFirstLayerFp16()
 				#new_shape = all_layers[0].dims
-				new_shape = (1, 3, 360, 640)
+				#new_shape = (1, 3, 360, 640)
+				new_shape = (1, 3, nn_img_size[1], nn_img_size[0])
 			lenlayer1 = len(layer1)
 
 			if debug:
@@ -224,8 +291,14 @@ with dai.Device(pipeline) as device:
 			output_colors = cv2.resize(output_colors, nn_img_size)
 
 			frame = show_deeplabv3p(output_colors, frame)
-			cv2.putText(frame, "Fps: {:.2f}".format(fps.fps()), (2, frame.shape[0] - 4), cv2.FONT_HERSHEY_TRIPLEX, 0.4, color=(255, 255, 255))
+			if args.rotatevideo != 'False':
+				frame = frame.transpose(1, 0, 2)
+			cv2.putText(cv2.UMat(frame), "Fps: {:.2f}".format(fps.fps()), (2, frame.shape[0] - 4), cv2.FONT_HERSHEY_TRIPLEX, 0.4, color=(255, 255, 255))
 			cv2.imshow("weighted", frame)
+
+			if args.outputvideo:
+				#out_cap.write(frame)
+				process.stdin.write(frame.tobytes())
 
 		if cv2.waitKey(1) == ord('q'):
 			break
