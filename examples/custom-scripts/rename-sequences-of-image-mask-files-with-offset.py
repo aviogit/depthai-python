ab1771a36bec252989513bf25b10245c924f6421
Andrea Ranieri <andrea.ranieri@cnr.it>
2021-11-10 12:39:24 +0100

    Added dry-run flag to rename-sequences-of-image-mask-files-with-offset.py

--- examples/custom-scripts/rename-sequences-of-image-mask-files-with-offset.py
+++ examples/custom-scripts/rename-sequences-of-image-mask-files-with-offset.py
@@ -4,8 +4,11 @@ import os
 import glob
 import argparse
 
+from argument_parser import define_boolean_argument, var2opt
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--offset', nargs='?', type=int, default=0, help="offset to be added/subtracted from the current filename")
+define_boolean_argument(parser, *var2opt('dry_run'), 'just show the modifications, don\'t actually rename any file', True)
 args = parser.parse_args()
 
 # 044997.jpg
@@ -21,4 +24,5 @@ for i in flist:
 	fprefix  = oldfname.replace(str(oldfbody), '')	# 0
 	newfname = fprefix + str(newfbody) + extension	# 044990.jpg
 	print(f'Renaming: {i} -> {newfname}')
-	#os.rename('a.txt', 'b.kml')
+	if not args.dry_run:
+		os.rename(i, newfname)
